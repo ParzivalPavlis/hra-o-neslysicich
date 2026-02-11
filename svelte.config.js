8be7a17141f10c1a5a1b50f23dd16c23aa9cefa0
Pavel Lacina <lacinapavel04@gmail.com>
2026-02-11 11:34:00 +0100

    🎨 enhance svelte configuration with preprocess and alias settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,18 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$components: './src/components',
+			$types: './src/lib/types',
+			$lib: './src/lib',
+			$styles: './src/styles'
+		}
+	}
+};
 
 export default config;
